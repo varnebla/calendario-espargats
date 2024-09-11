5e00a856237889203323427275ca6cde6f4ac9f3
Victor Arnedo Blanco <victor@MacBook-Air-de-Victor.local>
2024-09-11 18:06:18 +0200

    feat: add new sponsors, add new slider to opposite direction

---
+++ src/data/sponsors.ts
@@ -0,0 +1,107 @@
+export interface Sponsor {
+  name: string;
+  logo: string;
+  url?: string;
+}
+
+export const SPONSORS: Sponsor[] = [
+  {
+    name: 'BCN Retroclass',
+    logo: '/sponsors/bcn-retroclass.webp',
+    url: 'https://retroclassbcn.com/',
+  },
+  {
+    name: 'The Green Dog',
+    logo: '/sponsors/the-green-dog.webp',
+    url: 'https://www.instagram.com/thegreendog.esparreguera',
+  },
+  {
+    name: 'Terminal 20',
+    logo: '/sponsors/terminal20.webp',
+    url: 'https://terminal20.es/',
+  },
+  {
+    name: 'Calçats Valencia',
+    logo: '/sponsors/calcats-valencia.webp',
+    url: 'https://espardenyeriabarcelona.com',
+  },
+  {
+    name: 'Valero',
+    logo: '/sponsors/valero.webp',
+    url: 'https://www.instagram.com/valerofornesparreguera',
+  },
+  {
+    name: 'Pantoart',
+    logo: '/sponsors/pantoart.webp',
+    url: 'https://www.pantoart.com',
+  },
+  {
+    name: 'Los Nidos de David',
+    logo: '/sponsors/los-nidos-de-david.webp',
+    url: 'https://www.losnidosdedavid.com/',
+  },
+  {
+    name: 'Trocitos de scrap',
+    logo: '/sponsors/trocitos-de-scrap.webp',
+    url: 'https://www.instagram.com/trocitosdescrap',
+  },
+  {
+    name: 'El Badiu Scrap',
+    logo: '/sponsors/el-badiu-scrap.webp',
+    url: 'https://www.instagram.com/elbadiu_scrap',
+  },
+  {
+    name: 'Wald',
+    logo: '/sponsors/wald.webp',
+    url: 'https://waldesparraguera.es/',
+  },
+  {
+    name: 'Centre Veterinari Abrera',
+    logo: '/sponsors/centro-veterinario-abrera.webp',
+  },
+  {
+    name: 'Ana Marín Modista',
+    logo: '/sponsors/anamarin.webp',
+    url: 'https://www.instagram.com/anamarinmodista',
+  },
+  {
+    name: 'Activat ECO',
+    logo: '/sponsors/activat-eco.webp',
+    url: 'https://www.instagram.com/activat.eco',
+  },
+  {
+    name: 'Ana Web SEO',
+    logo: '/sponsors/ana-wse.webp',
+    url: 'https://www.instagram.com/webseoesparreguera',
+  },
+  {
+    name: 'Mohe',
+    logo: '/sponsors/gestoria-mohe.webp',
+    url: 'https://www.mo-he.es',
+  },
+  {
+    name: 'Jemaq Insumos',
+    logo: '/sponsors/jemaq-insumos.webp',
+    url: 'https://jemaq.com',
+  },
+  {
+    name: 'La Figaflor',
+    logo: '/sponsors/la-figaflor.webp',
+    url: 'https://www.lafigaflor.com',
+  },
+  {
+    name: 'Pet Shop Girls',
+    logo: '/sponsors/pet-shop-girls.webp',
+    url: 'https://www.instagram.com/pet.shop.girls',
+  },
+  {
+    name: 'Panadería Sopena',
+    logo: '/sponsors/sopena.webp',
+    url: 'https://www.instagram.com/panaderia-sopena',
+  },
+  {
+    name: 'Pet Sitters Team',
+    logo: '/sponsors/pet-sitters.webp',
+    url: 'https://www.instagram.com/petsittersteam',
+  },
+];
